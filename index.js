54e8f1bb3e9a9922a64b027fcb8da2ea24490fb6
Injae Lee <github email address>
2020-06-01 16:43:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+
+main.remove('main')
+
+var newHeader = document.createElement('h1')
+newHeader.id = "victory"
+
+name = "Injae"
+
+newHeader.innerHTML = `${name} is the champion`
+
+// var element = document.querySelector("h1#victory")
